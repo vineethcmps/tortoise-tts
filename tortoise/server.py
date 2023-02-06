4663ad230b82249f81f80c054c0df61b6b78d425
Vineeth C M <vineethcmps@gmail.com>
2023-02-06 11:23:10 +0000

    return voice sample used

--- tortoise/server.py
+++ tortoise/server.py
@@ -19,16 +19,17 @@ voice_to_gender = {
     "female": ["angie", "emma", "halle", "jlaw"]
 }
 
-def do_tts(text, gender, preset):
+def do_tts(text, gender, preset, voice_sel=None):
     name= str(uuid.uuid4())
-    voice_sel = random.choice(voice_to_gender[gender])
+    if voice_sel is None:
+        voice_sel = random.choice(voice_to_gender[gender])
     voice_samples, conditioning_latents = load_voices([voice_sel])
     gen, dbg_state = tts.tts_with_preset(text, k=1, voice_samples=voice_samples, conditioning_latents=conditioning_latents,
                                 preset=preset, use_deterministic_seed=None, return_deterministic_state=True, cvvp_amount=.0)
     # code to generate story based on the script
     out_file = f"/out/{uuid.uuid4()}.wav"
     torchaudio.save(out_file, gen.squeeze(0).cpu(), 24000, format="wav")
-    return out_file
+    return out_file, voice_sel
 
 
 @app.route("/get_voice", methods=["POST"])
@@ -36,9 +37,10 @@ def get_voice():
     text = request.json.get("text")
     gender = request.json.get("gender", "male")
     preset = request.json.get("preset", "fast")
+    voice_sel = request.json.get("voice_sel", None)
 
-    out_file = do_tts(text, gender, preset)
-    return jsonify({"out_file": out_file})
+    out_file, voice_sel = do_tts(text, gender, preset, voice_sel)
+    return jsonify({"out_file": out_file, "voice_sel": voice_sel})
 
 
 if __name__ == "__main__":
